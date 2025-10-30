92d9ac37118e4a2d6d09c8fb0e92daac85ad9a6c
Siddharth Bhattacharya <111268102+siddbhatt18@users.noreply.github.com>
2025-10-30 21:59:52 +0530

    Delete node js directory

--- node js/FigletDir/index.js
+++
@@ -1,9 +0,0 @@
-const figlet = require("figlet");
-figlet("Hello World", function(err, data) {
-    if(err) {
-        console.log("error");
-        console.dir(err);
-        return;
-    }
-    console.log(data);
-});
--- node js/import.js
+++
@@ -1,4 +0,0 @@
-import {sum, mul, pi} from "./math.js";
-console.log(sum(5, 6));
-console.log(mul(3, 4));
-console.log(pi);
