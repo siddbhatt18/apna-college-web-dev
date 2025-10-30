08d1df50c6d0b6a6c523de295886fa9e3b86b984
Siddharth Bhattacharya <111268102+siddbhatt18@users.noreply.github.com>
2025-10-30 18:46:12 +0530

    Delete node js directory

--- node js/Math.js
+++
@@ -1,7 +0,0 @@
-export const sum = function(a, b) {
-    return a + b;
-}
-export const mul = function(a, b) {
-    return a * b;
-}
-export const pi = 3.14;
--- node js/Process.js
+++
@@ -1,5 +0,0 @@
-console.log(process.argv);
-let args = process.argv;
-for(let i = 0; i < args.length; i++) {
-    console.log(args[i]);
-}
